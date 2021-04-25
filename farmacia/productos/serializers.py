3bb75c5ab9b2721a33897c38d01bd3465c949ea1
jpsanchez <jose.venados6@gmail.com>
2021-04-24 19:01:22 -0600

    Ya Gilda por favor

--- farmacia/productos/serializers.py
+++ farmacia/productos/serializers.py
@@ -4,10 +4,10 @@ from .models import Producto
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
-        fields = ['id', 'nombre', 'precio_compra', 'precio_venta',' descripcion' 'existencias']
+        fields = ['id', 'nombre', 'precio_compra', 'precio_venta',' descripcion', 'existencias']
         depth = 1
     
 class ProductoSerializerPost(serializers.ModelSerializer):
     class Meta:
         model = Producto
-        fields = ['id', 'nombre', 'precio_compra', 'precio_venta',' descripcion' 'existencias']
+        fields = ['id', 'nombre', 'precio_compra', 'precio_venta',' descripcion', 'existencias']
